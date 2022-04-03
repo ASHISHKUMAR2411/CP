05ecb9a88e2d412a651c6dbcad5abb81cceeaca5
Ashish Kumar <ashishkumar24112001@gmail.com>
2022-04-03 11:30:10 +0530

    template modified

--- a_template.cpp
+++ a_template.cpp
@@ -15,9 +15,10 @@
 #define fi(i, m) for (int i = 1; i <= m; i++)
 #define fir(i, n, m) for (int i = n; i <= m; i++)
 using namespace std;
-//? ARRAY
+list = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+{    //? ARRAY
 // array rotation
-//1.) juggling algorithm O(n) and O(1) for array rotation
+//1.) juggling algorithm O(n) and O(1) for array rotation}
 void jugglingAlgorithm(int a[], int n, int d)
 {
     // where d is the number of rotation of array should happen
@@ -102,6 +103,88 @@ void reversalAlgorithm(int arr[], int n, int d)
 }
 
 // ? NUMBER THEORY
+
+// The following function is used to find the number of prime number between 1 .... n  using block sieve which is using char array/vector which faster than bool array (in cache optimisation)
+
+int count_primes(int n)
+{
+    const int S = 10000;
+
+    vector<int> primes;
+    int nsqrt = sqrt(n);
+    vector<char> is_prime(nsqrt + 2, true);
+    for (int i = 2; i <= nsqrt; i++)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+            for (int j = i * i; j <= nsqrt; j += i)
+                is_prime[j] = false;
+        }
+    }
+
+    int result = 0;
+    vector<char> block(S);
+    for (int k = 0; k * S <= n; k++)
+    {
+        fill(block.begin(), block.end(), true);
+        int start = k * S;
+        for (int p : primes)
+        {
+            int start_idx = (start + p - 1) / p;
+            int j = max(start_idx, p) * p - start;
+            for (; j < S; j += p)
+                block[j] = false;
+        }
+        if (k == 0)
+            block[0] = block[1] = false;
+        for (int i = 0; i < S && start + i <= n; i++)
+        {
+            if (block[i])
+                result++;
+        }
+    }
+    return result;
+}
+
+// to find the prime number between l to r , it is segmented sieve 
+vector<char> segmentedSieveNoPreGen(long long L, long long R)
+{
+    vector<char> isPrime(R - L + 1, true);
+    long long lim = sqrt(R);
+    for (long long i = 2; i <= lim; ++i)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+
+// this method is using the same concept as above it is just that it store
+vector<char> segmentedSieve(long long L, long long R)
+{
+    // generate all primes up to sqrt(R)
+    long long lim = sqrt(R);
+    vector<char> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i)
+    {
+        if (!mark[i])
+        {
+            primes.emplace_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<char> isPrime(R - L + 1, true);
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
 
 // always use "\n" than endl ,
 
@@ -271,7 +354,7 @@ void primeFac(int N)
             N /= i;
         }
         if (cnt > 0)
-            factors.push_back((i * cnt));
+            factors.push_back((i));
     }
     if (N > 1)
         factors.push_back(N);
